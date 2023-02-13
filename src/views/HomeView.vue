1af1c6c473deaeb3b7841ebb72f513ca245ff045
Andrés F. Rodríguez <andresfra65@gmail.com>
2023-02-13 17:32:46 -0500

    subo repo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,7 +28,6 @@
                   <span> Nombre completo</span>
                   <input type="text">
                 </div>
-                
                 <div class="footerHomeBoxSendInput" >
                   <span> Producto de interes</span>
                   <input type="text">
