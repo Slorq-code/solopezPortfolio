559d6619584a1b16f65cf08a8d6a94ace5c0b05a
Andrés F. Rodríguez <andresfra65@gmail.com>
2023-02-25 12:21:03 -0500

    update: add img background

--- src/views/DesignPublicity.vue
+++ src/views/DesignPublicity.vue
@@ -4,9 +4,14 @@
         <div class="uperTitle" >
             <img src="" alt="">
             <span>
-                Ofrecemos servicios de diseño pu
+                Ofrecemos servicios de diseño publiciitario para piezas digitales y físicas, incluyendo volantes, pendones y tarjetas impresas con un ennfoque en la calidad y el impacto visual.
             </span>
         </div>
+        <section>
+            <div class="grilla" >
+                
+            </div>
+        </section>
 
     </div>
 </template>
@@ -25,7 +30,9 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: red;
+    background-image: url("../assets/imagenes/Fondo-de-diseño.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 
 </style>
