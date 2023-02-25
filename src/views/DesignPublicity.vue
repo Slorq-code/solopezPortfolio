5f9e672c5e6b22293a90d2318c706d446647dd31
Andrés F. Rodríguez <andresfra65@gmail.com>
2023-02-25 12:49:21 -0500

    update: header page design

--- src/views/DesignPublicity.vue
+++ src/views/DesignPublicity.vue
@@ -2,7 +2,7 @@
     <div class="designPublicityContainer" >
 
         <div class="uperTitle" >
-            <img src="../assets/imagenes/DisenoPublicidad.png" alt="logo">
+            <img src="@/assets/imagenes/DisenoPublicidad.png" alt="logo">
             <span>
                 Ofrecemos servicios de diseño publiciitario para piezas digitales y físicas, incluyendo volantes, pendones y tarjetas impresas con un ennfoque en la calidad y el impacto visual.
             </span>
@@ -30,14 +30,19 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-image: url("../assets/imagenes/Fondo-de-diseño.jpg");
+    background-image: url("@/assets/imagenes/Fondo-de-diseño.jpg");
     background-repeat: no-repeat;
     background-size: cover;
+    position: relative;
 }
 
 .uperTitle {
     display: flex;
-    
+    position: absolute;
+    top: 0;
+    height: 10%;
+    width: 100%;
+    background-color: red;
 }
 
 </style>
